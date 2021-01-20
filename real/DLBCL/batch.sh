144d1922233e7ea656857da5a734e365002f7fe3
Lei-D <dlei1023@gmail.com>
2021-01-19 23:36:21 -0800

    Add files via upload

---
+++ real/DLBCL/batch.sh
@@ -0,0 +1,11 @@
+#PBS -N DLBCL
+#PBS -l nodes=1:ppn=1,walltime=6:00:00
+#PBS -M dinglei@indiana.edu
+
+
+module load nlopt
+module load curl
+module load java/1.7.0_51
+module load r/3.6.0
+cd Desktop/Lei/AIMER2/ISMB/real/DLBCL/
+R CMD BATCH --no-save --no-restore analysis.R 
